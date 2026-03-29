d09717a53c0dc67748db027e66f1239a70f696a8
Kyrsten Daphne <kyrsten.daphne@gmail.com>
2026-03-29 10:22:19 +0800

    Add project files.

---
+++ AdetAct2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdetAct2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> toDoList = new List<string>();
+            string userInput = "";
+
+            Console.WriteLine("To-Do List");
+
+            while (userInput != "exit")
+            {
+                Console.WriteLine("\nchoose an option:");
+                Console.WriteLine("1. Add a task | 2. View | 3. Clear | Type 'bye' to exit");
+                Console.WriteLine("Option: ");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("enter new task: ");
+                        string task = Console.ReadLine();
+                        toDoList.Add(task);
+                        Console.WriteLine("task added.");
+                        break;
+                    case "2":
+                        Console.WriteLine("to-Do list:");
+                        foreach (string item in toDoList)
+                        {
+                            Console.WriteLine("- " + item);
+                        }
+                        break;
+                    case "3":
+                        toDoList.Clear();
+                        Console.WriteLine("to-do list cleared.");
+                        break;
+                    case "bye":
+                        userInput = "exit";
+                        Console.WriteLine("bye!");
+                        break;
+                    default:
+                        Console.WriteLine("invalid option. pls try again.");
+                        break;
+                } 
+
+            }
+        }
+    }
+}
